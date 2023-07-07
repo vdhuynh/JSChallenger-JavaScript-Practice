75134df13b844699e71ef1d92bdc0a6104ebe4c2
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-07 05:35:30 -0500

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,20 @@
+const text = 'hello world';
+console.log('The value of text is: ' + text);
+
+const textOne = 'Hello, ';
+const textTwo = "it's ";
+const textThree = `me`;
+console.log(textOne, textTwo, textThree);
+
+The author's solution
+const textOne = 'Hello';
+const textTwo = 'World';
+const combined = textOne + ' ' + textTwo;
+console.log(combined);
+
+const word = 'interesting';
+const sentence = `She had an ${word} idea.`;
+console.log(sentence);
+
+const sentence = `The ice cream costs ${1+1} dollars`;
+console.log(sentence);
