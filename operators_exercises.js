e45215cad1c7360f0633fe7eee0cd649a5040144
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-06 22:42:58 -0500

    Create operators_exercises.js

---
+++ operators_exercises.js
@@ -0,0 +1,74 @@
+const numOne = 5;
+const numTwo = 5;
+console.log(numOne == numTwo);
+
+const numOne = 5;
+const numTwo = 6;
+console.log(numOne != numTwo);
+
+const numOne = 6;
+const numTwo = 5;
+console.log(numOne > numTwo);
+
+const numOne = 1;
+const numTwo = 2;
+console.log(numOne < numTwo);
+
+const numOne = 3;
+const numTwo = 2;
+const numThree = 2;
+console.log(numOne >= numTwo, numTwo >= numThree);
+
+const numOne = 1;
+const numTwo = 1;
+const numThree = 2;
+console.log(numOne <= numTwo, numTwo <= numThree);
+
+const numOne = 5;
+const numTwo = 5;
+console.log(numOne === numTwo);
+
+const numOne = 5;
+const numTwo = '5';
+console.log(numOne !== numTwo, numOne == numTwo);
+
+const numOne = 13;
+const numTwo = 13;
+const result = numOne > 12 && numTwo > 12;
+console.log(result);
+
+const numOne = 5;
+const numTwo = 13;
+const result = numOne > 12 || numTwo > 12;
+console.log(result);
+
+const numOne = 4;
+const numTwo = 6;
+const sum = numOne + numTwo;
+console.log(sum == 10);
+
+const numOne = 9;
+const numTwo = 4;
+const dif = numOne - numTwo;
+console.log(dif == 5);
+
+const numOne = 9;
+const numTwo = 9;
+const product = numOne * numTwo;
+console.log(product == 81);
+
+const numOne = 81;
+const numTwo = 9;
+const quotient = numOne / numTwo;
+console.log(quotient == 9);
+
+const numOne = 2;
+const numTwo = 3;
+const res = numOne**numTwo;
+console.log(res == 8);
+
+const numOne = 13;
+const numTwo = 5;
+const remainder = numOne%numTwo
+console.log(res === 3);
+
