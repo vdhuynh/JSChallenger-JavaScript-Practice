3004340035fae55f335210ca0084a7681b3dac38
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-07 10:47:52 -0500

    Create object.js

---
+++ object.js
@@ -0,0 +1,24 @@
+let lifespan;
+const animal = {
+    species: 'Lion',
+    habitat: 'Savannah',
+    lifespan: 10,
+    family: 'Felidae'
+};
+lifespan = animal.lifespan;
+console.log(`The Lion has a lifespan of ${lifespan} years`);
+
+const person = {
+    age: 20,
+    name: 'Tom'
+};
+console.log(`${person.name} is ${person.age} years old.`);
+
+const car = {
+    make: 'Toyota',
+    model: 'Corolla',
+    year: 2020,
+    color: 'silver'
+};
+console.log(`The car was built in ${car.year}`);
+
