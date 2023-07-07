2ee5ee0c4edbb55d456f50d030d835fab08548b0
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-06 19:10:25 -0500

    Create variables_exercises.js

---
+++ variables_exercises.js
@@ -0,0 +1,28 @@
+let num = 1;
+num = 2;
+console.log(num);
+
+let num;
+num = 2;
+console.log(num);
+
+let numOne = 5;
+let numTwo = numOne;
+console.log(numTwo);
+
+let numOne = 5;
+let numTwo = numOne;
+console.log(numTwo);
+
+const num = 5;
+console.log(num);
+
+const num = 5;
+console.log(num);
+
+const num = 5;
+console.log('The value of num is: ' + num);
+
+let num = 1;
+num = 2;
+console.log(num);
